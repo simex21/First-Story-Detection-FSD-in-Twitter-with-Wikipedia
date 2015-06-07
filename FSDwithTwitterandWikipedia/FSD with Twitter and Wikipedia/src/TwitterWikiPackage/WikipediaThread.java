fa26899036516646e1b3af455bc7a6e3b1ea1ef5
simex21 <simex2010@gmail.com>
2015-06-07 16:28:25 +0200

    Update WikipediaThread.java

--- FSDwithTwitterandWikipedia/FSD with Twitter and Wikipedia/src/TwitterWikiPackage/WikipediaThread.java
+++ FSDwithTwitterandWikipedia/FSD with Twitter and Wikipedia/src/TwitterWikiPackage/WikipediaThread.java
@@ -2,39 +2,24 @@ package TwitterWikiPackage;
 
 import java.io.IOException;
 
-class RunnableDemo implements Runnable {
-	   private Thread t;
-	   private String threadName;
-	   
-	   RunnableDemo(){
-
-	   }
-	   public void run() {
-	      try {
-				WikipediaEventDetector.wikiEventDetector();
-	    	    Thread.sleep(50);
-	     } catch (InterruptedException | IOException e) {
-	         System.out.println("Wikipedia event detection interrupted.");
-	     }
-	   }
-	   
-	   public void start ()
-	   {
-	      if (t == null)
-	      {
-	         t = new Thread (this);
-	         t.start ();
-	      }
-	      System.out.println("thread state is: "+t.getState());
-	   }
-
+public class WikipediaThread extends Thread {
+	public WikipediaThread(){
+		//create a new thread for wikipedia event detection
+		super("Wiki Thread");
+		System.out.println("Starting Wikipedia Event Detection thread...");
+		start(); //start the thread
 	}
-
-	public class WikipediaThread {
-	   //public static void main(String args[]) {
-		public static void wikiThread(){
-	   
-	      RunnableDemo R1 = new RunnableDemo();
-	      R1.start();
-	   }   
+	//This is the entry point for the Wiki thread
+	public void run(){
+		try{
+			WikipediaEventDetector.wikiEventDetector();
+			Thread.sleep(50);
+		}catch(IOException e){
+			System.out.println("Necessary files were not found.");
+		}
+		catch(InterruptedException a){
+			System.out.println("Wikipedia event detection thread interrupted.");
+		}
+		System.out.println("Exiting Wikipedia Event detection thread.");
 	}
+}
